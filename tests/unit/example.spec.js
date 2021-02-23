67b935e9f04dce70a981341394a13e8e102c3b83
PablinVaz <57219639+PablinVaz@users.noreply.github.com>
2021-02-23 14:45:34 +0100

    test de prueba

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,23 @@
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
+import TestComponent from "@/components/TestComponent.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+      propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
+  });
+});
+
+describe("TestComponent.vue", () => {
+  it("should be pass when render slots", () => {
+    const title = "<span>Title</span>";
+    const wrapper = shallowMount(TestComponent, {
+      slots: { title },
+    });
+    expect(wrapper.html()).toMatch(title);
   });
 });
